697879f21976e7aaca6c5befd5055cfe08e67451
kisbayu <kisbayu.k.a@gmail.com>
2022-10-02 13:58:45 +0700

    edit activities

--- app/src/main/java/com/papb/papb_layout_and_navigation/MainActivity.kt
+++ app/src/main/java/com/papb/papb_layout_and_navigation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.papb.papb_layout_and_navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 
@@ -9,14 +10,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(applicationContext,"onCreate",Toast.LENGTH_SHORT).show()
+    }
 
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(applicationContext,"onStart",Toast.LENGTH_SHORT).show()
     }
 
     override fun onResume() {
         super.onResume()
+        Toast.makeText(applicationContext,"onResume",Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Toast.makeText(applicationContext,"onDestroy",Toast.LENGTH_SHORT).show()
     }
 }
